3a76470c67debe5908afbeecf83919c388bb3e20
Greenderix <solozobov.e.d@edu.mirea.ru>
2021-09-13 12:08:17 +0300

    Create Square.java

---
+++ src/Practic3/first/Square.java
@@ -0,0 +1,45 @@
+package Practic3.first;
+
+public class Square extends Rectangle {
+
+    protected double side;
+
+    public Square() {
+        this.color = "RED";
+        this.filled = true;
+        this.side = 0;
+    }
+    public Square(double side) {
+        this.color = "RED";
+        this.filled = true;
+        this.side = side;
+    }
+    public Square(String color, boolean filled, double side) {
+        this.color = color;
+        this.filled = filled;
+        this.side = side;
+    }
+    public double getSide() {
+        return side;
+    }
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                ", side=" + side +
+                '}';
+    }
+}
